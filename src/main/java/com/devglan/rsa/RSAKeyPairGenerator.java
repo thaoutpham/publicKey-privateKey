7f7e80474720182638fb468b000eee519ab72e02
thaoptbeetsoft <thaopt@beetsoft.com.vn>
2022-05-12 08:27:19 +0700

    public key end private key

---
+++ src/main/java/com/devglan/rsa/RSAKeyPairGenerator.java
@@ -0,0 +1,38 @@
+package com.devglan.rsa;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.*;
+import java.util.Base64;
+
+public class RSAKeyPairGenerator {
+
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public RSAKeyPairGenerator() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(1024);
+        KeyPair pair = keyGen.generateKeyPair();
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
+    }
+
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        RSAKeyPairGenerator keyPairGenerator = new RSAKeyPairGenerator();
+        byte[] key1 = keyPairGenerator.getPublicKey().getEncoded();
+        byte[] key2 = keyPairGenerator.getPrivateKey().getEncoded();
+        System.out.println(Base64.getEncoder().encodeToString(keyPairGenerator.getPublicKey().getEncoded()));
+        System.out.println(Base64.getEncoder().encodeToString(keyPairGenerator.getPrivateKey().getEncoded()));
+    }
+}
